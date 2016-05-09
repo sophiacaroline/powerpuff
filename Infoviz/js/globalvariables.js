f9339813081a43d4e49ad55b4c8da3044b2d718f
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-05-09 13:31:48 +0200

    Even more space jam tests.

---
+++ Infoviz/js/globalvariables.js
@@ -0,0 +1,31 @@
+// Color of the party
+COLOR = new Map();
+COLOR.set("Moderaterna", "blue");
+COLOR.set("Centerpartiet", "#016A3A");
+COLOR.set("Folkpartiet", "#0094D7");
+COLOR.set("Kristdemokraterna", "#08519c");
+COLOR.set("Miljöpartiet", "#53A045");
+COLOR.set("Socialdemokraterna", "#ED1B34");
+COLOR.set("Vänsterpartiet", "#a50f15");
+COLOR.set("Sverigedemokraterna", "#DDDD00");
+COLOR.set("Övriga partier", "gray");
+COLOR.set("Odefinierad", "black");
+
+// Map for mining
+MININGMAP = new Map();
+MININGMAP.set("Vald Kommun", "orange");
+MININGMAP.set("Mest lika", "#00cc99");
+MININGMAP.set("Minst lika", "purple");
+
+// Array for mining
+MININGARRAY = ["Vald Kommun", "Mest lika", "Minst lika"];
+
+ELECTIONYEARSARRAY = [1973, 1976, 1979, 1982, 1985, 1988, 1991, 1994, 1998, 2002, 2006, 2010, 2014];
+
+REGIONARRAY = [];
+
+PARTYLEGENDLENGTH = 5;
+
+// Startup
+DEFAULTREGION = "Karlstad";
+DEFAULTYEAR = "2014";
